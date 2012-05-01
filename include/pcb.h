c2174abbe47c0765658d3ff77e916bec1fb06ea3
Pedro Vanzella <pedro@pedrovanzella.com>
2012-05-01 19:20:40 -0300

    status is defined by what list pcb is in

--- include/pcb.h
+++ include/pcb.h
@@ -4,11 +4,8 @@
 #define _XOPEN_SOURCE /* To compile under OSX */
 #include <ucontext.h>
 
-enum status_t {ready, blocked};
-
 struct pcb_t {
 	int pid;
-	enum status_t status;
 	ucontext_t* context; /* Processes' status */
 };
 
