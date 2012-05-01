e59bf9cfbf5a01b35803228ff7ca30848d9d5b49
Pedro Vanzella <pedro@pedrovanzella.com>
2012-05-01 11:34:03 -0300

    added pcb type

---
+++ include/pcb.h
@@ -0,0 +1,13 @@
+#ifndef _PCB_H_
+#define _PCB_H_
+#include <ucontext.h>
+
+typedef status_t enum {ready, running, blocked}
+
+typedef pcb_t struct {
+	int pid;
+	status_t status;
+	ucontext_t context; /* Processes' status */
+};
+
+#endif /* _PCB_H_ */
