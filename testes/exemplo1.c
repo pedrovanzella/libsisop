97842d248be92a7690722bbd749e2a56c37fa7f2
Pedro Vanzella <pedro@pedrovanzella.com>
2012-05-02 14:22:25 -0300

    updated example 1

--- testes/exemplo1.c
+++ testes/exemplo1.c
@@ -1,8 +1,13 @@
 #include "../include/unucleo.h"
 #include <stdio.h>
 
+void say_hello()
+{
+	printf("Hello, World!\n");
+}
+
 int main(int argc, char* argv[])
 {
-	printf("Hello World!\n");
+	mproc_create(2, (void *)say_hello, (void *)NULL);
 	return 0;
 }
